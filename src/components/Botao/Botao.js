65055691f87a26d449769de9f84cccffd4fba8f5
Abrahão Lyncon <abrahaolyncon12@gmail.com>
2024-01-16 15:02:10 -0400

    Adicionei botao e alterei algumas coisas pequenas no CSS

---
+++ src/components/Botao/Botao.js
@@ -0,0 +1,11 @@
+import "./Botao.css"
+
+import React from 'react'
+
+const Botao  = ({texto}) => {
+  return (
+    <button className="botao">{texto}</button>
+  )
+}
+
+export default Botao 
--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -2,6 +2,7 @@ import "./Formulario.css"
 import React from 'react'
 import CampoTexto from '../CampoTexto';
 import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao/Botao";
 
 const Formulario = () => {
 
@@ -22,8 +23,9 @@ const Formulario = () => {
                 <h2>Preencha dos dados para criar o card do Colaborador</h2>
                 <CampoTexto nome="Nome" placeholder="Digite seu nome" />
                 <CampoTexto nome="Cargo" placeholder="Digite seu cargo" />
-                <CampoTexto nome="Imagem" placeholder="Endereço da Imagem" />
+                <CampoTexto nome="Imagem" placeholder="Endereço da Imagem (URL - GitHub)" />
                 <ListaSuspensa nome="Time" itens={times}  />
+                <Botao texto="Mame" />
             </form>
         </section>
     </div>
